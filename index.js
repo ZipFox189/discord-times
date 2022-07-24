e9d0472c444be8baff0613754c10ddd180129c10
ZipFox189 <46225636+ZipFox189@users.noreply.github.com>
2022-07-25 00:42:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,62 +9,9 @@ client.on("ready", () => {
 });
 
 client.on("message", (msg) => {
-  let isWelcomeMessage = msg.type === "GUILD_MEMBER_JOIN";
-
-  if (isWelcomeMessage) {
-    msg.author.send(`Welcome to the server, ${msg.author.username}!`);
-    client.channels.cache
-      .get("969519390951366676")
-      .send(`${msg.author.username} has joined the server!`);
+  if (msg.content === "Hello") {
+    sendMessage("Hello!");
   }
-
-  let prefix = "!";
-  let message = msg.content;
-
-  let channel = msg.channelId;
-  let botChannel = "968539510923210795";
-
-  const sendMessage = (message) => {
-    client.channels.cache.get(botChannel).send(message);
-  };
-
-  if (msg.author.username !== "IKeepTheBugsAway") {
-    if (message.includes("no")) {
-      msg.delete();
-      sendMessage(`${msg.author.username} said no!`);
-    }
-    
-    if (channel === botChannel) {
-      if (message.startsWith(prefix)) {
-        const command = message.slice(prefix.length).split(" ")[0];
-
-        let admins = ["Michael Kitas#4423"];
-
-        // let isAdmin = `${msg.author.username}#${msg.author.discriminator}` in admins;
-        let isAdmin = msg.member.roles.cache.find(
-          (role) => role.name === "Admin"
-        );
-
-        switch (command) {
-          case "stats":
-            isAdmin
-              ? sendMessage(`This server has ${msg.guild.memberCount} members`)
-              : sendMessage("You are not an admin");
-            break;
-
-          case "help":
-            sendMessage("This is a help command");
-            break;
-        }
-      }
-    } else {
-      msg.channel.send("This is not a bot channel");
-    }
-  }
-
-if (msg.content === "Hello") {
-  sendMessage("Hello!");
-}
 });
 
 client.login(process.env.token);
