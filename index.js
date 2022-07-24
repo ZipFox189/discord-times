22e2d5324d5cc2764e5724e7cdd76a6954de59ba
ZipFox189 <46225636+ZipFox189@users.noreply.github.com>
2022-07-25 01:36:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,6 @@ const client = new Client({
 });
 
 client.on("message", (msg) => {
-  let isWelcomeMessage = msg.type === "GUILD_MEMBER_JOIN";
-
-  if (isWelcomeMessage) {
-    msg.author.send(`Welcome to the server, ${msg.author.username}!`);
-    client.channels.cache
-      .get("969519390951366676")
-      .send(`${msg.author.username} has joined the server!`);
-  }
 
   let prefix = "!";
   let message = msg.content;
@@ -23,40 +15,32 @@ client.on("message", (msg) => {
   const sendMessage = (message) => {
     client.channels.cache.get(botChannel).send(message);
   };
-
-  if (msg.author.username !== "IKeepTheBugsAway") {
-    if (message.includes("no")) {
-      msg.delete();
-      sendMessage(`${msg.author.username} said no!`);
-    }
     
-    if (channel === botChannel) {
-      if (message.startsWith(prefix)) {
-        const command = message.slice(prefix.length).split(" ")[0];
+   if (channel === botChannel) {
+     if (message.startsWith(prefix)) {
+       const command = message.slice(prefix.length).split(" ")[0];
 
-        let admins = ["JJCode#3136"];
+       let admins = ["JJCode#3136"];
 
-        // let isAdmin = `${msg.author.username}#${msg.author.discriminator}` in admins;
-        let isAdmin = msg.member.roles.cache.find(
-          (role) => role.name === "Admin"
-        );
+       // let isAdmin = `${msg.author.username}#${msg.author.discriminator}` in admins;
+       let isAdmin = msg.member.roles.cache.find(
+         (role) => role.name === "Admin"
+       );
 
-        switch (command) {
-          case "stats":
-            isAdmin
-              ? sendMessage(`This server has ${msg.guild.memberCount} members`)
-              : sendMessage("You are not an admin");
-            break;
+       switch (command) {
+         case "stats":
+           isAdmin
+             ? sendMessage(`This server has ${msg.guild.memberCount} members`)
+             : sendMessage("You are not an admin");
+           break;
 
-          case "help":
-            sendMessage("This is a help command");
-            break;
-        }
-      }
-    } else {
-      msg.channel.send("This is not a bot channel");
-    }
-  }
+         case "help":
+           sendMessage("This is a help command");
+           break;
+       }
+     }
+   } 
+ }
 
   // if (msg.content === "Hello") {
   //   sendMessage("Hello!");
