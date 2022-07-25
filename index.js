9ef2da9e6049df2bab0b94b0154b904a3cffa3eb
ZipFox189 <46225636+ZipFox189@users.noreply.github.com>
2022-07-25 09:52:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,35 +16,9 @@ client.on("message", (msg) => {
     client.channels.cache.get(botChannel).send(message);
   };
     
-   if (channel === botChannel) {
-     if (message.startsWith(prefix)) {
-       const command = message.slice(prefix.length).split(" ")[0];
-
-       let admins = ["JJCode#3136"];
-
-       // let isAdmin = `${msg.author.username}#${msg.author.discriminator}` in admins;
-       let isAdmin = msg.member.roles.cache.find(
-         (role) => role.name === "Admin"
-       );
-
-       switch (command) {
-         case "stats":
-           isAdmin
-             ? sendMessage(`This server has ${msg.guild.memberCount} members`)
-             : sendMessage("You are not an admin");
-           break;
-
-         case "help":
-           sendMessage("This is a help command");
-           break;
-       }
-     }
-   } 
- }
-
-  // if (msg.content === "Hello") {
-  //   sendMessage("Hello!");
-  // }
+  if (msg.content === "Hello") {
+    sendMessage("Hello!");
+  }
 });
 
 client.login(process.env.token);
